2325fd1547a61c5544318617e2b61f55b2977266
Vertipae <33002993+Vertipae@users.noreply.github.com>
2018-08-16 10:55:14 +0300

    3.3

--- index.js
+++ index.js
@@ -32,6 +32,17 @@ app.get('/info', (req, res) => {
     res.send(`<p> Puhelinluettelossa on ${persons.length} henkilön tiedot </p><p>${new Date()}</p>`)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
