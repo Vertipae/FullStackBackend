52237d0280a05f4d800b9434c6b6d2783046b270
Vertipae <33002993+Vertipae@users.noreply.github.com>
2018-09-16 14:14:19 +0300

    3.14

--- models/person.js
+++ models/person.js
@@ -4,10 +4,26 @@ const mongoose = require('mongoose')
 const url = process.env.MONGODB_URI
 console.log(url)
 mongoose.connect(url)
-// Perustetaan mongoose modeli
-const Person = mongoose.model('Person', {
+
+
+const personSchema = new mongoose.Schema({
     name: String,
     number: String
 })
+
+personSchema.statics.format = function (person) {
+    return ({
+        name: person.name,
+        number: person.number,
+        id: person._id
+    })
+}
+
+const Person = mongoose.model('Person', personSchema)
+// Perustetaan mongoose modeli
+// const Person = mongoose.model('Person', {
+//     name: String,
+//     number: String
+// })
 
 module.exports = Person
